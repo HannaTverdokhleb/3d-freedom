21dffa0e0f2ed06fe41df42bb70476f7b937ccca
HannaTverdokhleb <84972134+HannaTverdokhleb@users.noreply.github.com>
2021-08-14 09:33:11 +0000

    Deploying to gh-pages from @ HannaTverdokhleb/3d-freedom@7e383d4d2098299eeb14785c590e9b91118d6602 🚀

--- src.ad5590a7.js
+++ src.ad5590a7.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/3d-freedom/src.f8e0f881.js.map
+//# sourceMappingURL=/3d-freedom/src.ad5590a7.js.map
